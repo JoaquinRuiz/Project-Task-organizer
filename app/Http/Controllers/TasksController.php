f8e2aa9e4588e3296aa3b3c1ecb0cec92a5152d6
Joaquín Ruiz <jokioki@gmail.com>
2015-02-28 22:22:04 +0000

    [init]

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,95 @@
+<?php namespace App\Http\Controllers;
+
+use App\Task;
+use App\Project;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class TasksController extends Controller {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+     * @param  Project $project
+     * @return Response
+	 */
+	public function index(Project $project)
+	{
+		//
+
+        return view('tasks.index', compact('project'));
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+     * @param  Project $project
+     * @return Response
+	 */
+	public function create(Project $project)
+	{
+		//
+        return view('tasks.create', compact('project'));
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  Task $task
+     * @param  Project $project
+     * @return Response
+	 */
+	public function show(Project $project, Task $task)
+	{
+		//
+        return view('tasks.show', compact('project','task'));
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+     * @param  Task $task
+     * @param  Project $project
+	 * @return Response
+	 */
+	public function edit(Project $project, Task $task)
+	{
+		//
+        return view('tasks.edit', compact('project', 'task'));
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  Task $task
+	 * @return Response
+	 */
+	public function update(Task $task)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  Task $task
+	 * @return Response
+	 */
+	public function destroy(Task $task)
+	{
+		//
+	}
+
+}
